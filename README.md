c2b16b23116e75b966ae826f7754c5b8a0ce66f0
IDLOCKED <depapepestart@gmail.com>
2016-01-25 12:59:47 +0900

    初回コミット

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ViewHatenaBookmark
-Chrome Extension ViewHatenaBookmark
+ViewHatenaBookmark Chrome Extension
+
+アイコンをクリックして開いているページのはてなブックマークを開くChrome Extensionです。
+オフィシャルのExtensionでは、はてなブックマークのページに行く為に二回クリックする必要があり不便でしたので作りました。
+なお、ブックマーク機能はありません。
+
+開いてるページのブクマ数もアイコンに表示します。
+はてなブックマーク件数取得API - Hatena Developer Center
+http://developer.hatena.ne.jp/ja/documents/bookmark/apis/getcount
+を使っています。
+
+#LICENSE
+MIT License
+
+#謝辞
+[フリー]布風はてブアイコン[自作]
+http://the-gremlin.me/icon/750
+アイコンをお借りしました。
---
+++ js/background.js
@@ -0,0 +1,38 @@
+$(function(){
+    var url;
+
+    // タブ切り替えた時
+    chrome.tabs.onActivated.addListener(function(tabId) {
+        createURL();
+    });
+    // タブ読み込み時
+    chrome.tabs.onUpdated.addListener(function(tabId, opt) {
+        createURL();
+    });
+    // タブ作成時
+    chrome.tabs.onCreated.addListener(function(tab){
+        createURL();
+    });
+    // 右上のアイコン押下時
+    chrome.browserAction.onClicked.addListener(function(){
+        window.open('http://b.hatena.ne.jp/entry/' + url);
+    });
+
+    function createURL(){
+        // 今開いてるタブの情報を取得
+        chrome.tabs.query({"active": true, "currentWindow": true}, function(tabs){
+            // はてブ数チェック
+            $.get("http://api.b.st-hatena.com/entry.count?url=" + tabs[0].url, function(count){
+                // カウント数をバッジにセット
+                chrome.browserAction.setBadgeText({text:count});
+            });
+
+            // "http://"を削除、"https://"の時は"s/"に置換
+            url = tabs[0].url.replace(/^(http:\/\/)/,"");
+            if(url === tabs[0].url){
+                url = url.replace(/^(https:\/\/)/,"s/");
+            }
+        });
+    }
+
+});
